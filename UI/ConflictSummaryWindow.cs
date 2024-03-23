f6bfc5878271389050dc4620a9a078935b605822
Dominic Nguyen <domynguyen_4@hotmail.com>
2024-03-23 15:06:52 -0700

    Conflict Window Design

---
+++ UI/ConflictSummaryWindow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using vatsys;
+
+namespace AuroraLabelItemsPlugin
+{
+    public partial class Form1 : BaseForm
+    {
+        AtopPlugin.Conflict.ConflictProbe.ConflictData cpar = new AtopPlugin.Conflict.ConflictProbe.ConflictData();
+
+        public Form1() 
+        {
+
+            InitializeComponent();
+        }
+
+        public void addRow(string cs1, string cs2)
+        {
+            
+        }
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
